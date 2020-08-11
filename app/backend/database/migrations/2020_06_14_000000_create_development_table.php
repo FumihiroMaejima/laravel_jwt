cbf5422e8dc58a6837cd03b777362f40249b8f5a
Fumihiro_Maejima <testerwatson1101@gmail.com>
2020-08-11 23:06:27 +0900

    create admin log table.

--- app/backend/database/migrations/2020_06_14_000000_create_development_table.php
+++ app/backend/database/migrations/2020_06_14_000000_create_development_table.php
@@ -13,6 +13,7 @@ class CreateDevelopmentTable extends Migration
      */
     public function up()
     {
+        // 所属部署テーブル
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string('name')->comment('所属部署');
@@ -21,12 +22,26 @@ class CreateDevelopmentTable extends Migration
             $table->softDeletes();
         });
 
+        // 所属チームテーブル
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->foreignId('department_id')->comment('所属部署ID')->constrained()->onDelete('cascade');
             // $table->foreignId('user_id')->constrained('users'); // テーブル名を指定する場合
             $table->string('name')->comment('所属チーム');
             $table->string('code')->unique()->comment('チームコード');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        // 管理者ログテーブル
+        Schema::create('admin_log', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->comment('管理者ID')->constrained();
+            $table->string('path')->comment('リクエストURL');
+            $table->string('method')->comment('リクエストメソッド');
+            $table->tinyInteger('status')->comment('ステータスコード');
+            $table->timestamp('start_at', 0)->comment('アクション開始時刻');
+            $table->timestamp('end_at', 0)->comment('アクション終了時刻');
             $table->timestamps();
             $table->softDeletes();
         });
